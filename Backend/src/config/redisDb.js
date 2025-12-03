11ffcb92a3357973ed01e626b12ac4de17a59259
HarshSoni1801 <113286848+HarshSoni1801@users.noreply.github.com>
2025-12-03 12:45:43 +0530

    Fix Redis connection host and port configuration

--- Backend/src/config/redisDb.js
+++ Backend/src/config/redisDb.js
@@ -4,8 +4,8 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASSWORD, 
     socket: {
-        host: 'redis-16439.c85.us-east-1-2.ec2.cloud.redislabs.com:16439',
-        port: 12036
+        host: 'redis-16439.c85.us-east-1-2.ec2.cloud.redislabs.com',
+        port: 16439
     }
 });
 
