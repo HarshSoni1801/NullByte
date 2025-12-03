240232b288e522c254cdfe12e02b027e5f30f58d
HarshSoni1801 <113286848+HarshSoni1801@users.noreply.github.com>
2025-12-03 12:42:45 +0530

    Update Redis host configuration

--- Backend/src/config/redisDb.js
+++ Backend/src/config/redisDb.js
@@ -4,7 +4,7 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASSWORD, 
     socket: {
-        host: 'redis-12036.c305.ap-south-1-1.ec2.redns.redis-cloud.com',
+        host: 'redis-16439.c85.us-east-1-2.ec2.cloud.redislabs.com:16439',
         port: 12036
     }
 });
